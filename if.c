036a0f03bcf2a817b191966bb1f4b7d8ec1eb95c
Anmol Singh <97746903+AnmolSingh24@users.noreply.github.com>
2022-01-24 07:36:41 -0800

    Add files via upload

---
+++ if.c
@@ -0,0 +1,14 @@
+#include"stdio.h"
+#include"conio.h"
+main()
+{
+    int a;
+    printf("enter the number: ");
+    scanf("%d",&a);
+    if (a%2==0)
+    {
+        printf("\nthe number you entered is an even number");
+
+    }
+    
+}
